35e5e931d32a2a9e79ecdd8ef6e79e6c4dc4771e
Oleg V Kolesnikov <okolesnikov@itera-research.com>
2023-12-28 20:19:48 +0200

    Install json output

--- webservice/api/serializers.py
+++ webservice/api/serializers.py
@@ -1,17 +1,19 @@
-import logging
 import os
-import tempfile
 import zipfile
+import logging
 
+import tempfile
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-log = logging.getLogger("deepview")
 
+log = logging.getLogger("deepview")
 
 class ProjectUploadSerializer(serializers.Serializer):
     project_archive = serializers.FileField()
-    mode = serializers.ChoiceField(choices=["time", "memory"], required=True)
+    mode = serializers.ChoiceField(choices=["analysis", "time", "memory"], required=True)
+    analysis_types = serializers.MultipleChoiceField(choices=[("--all", "All Analysis"),("--measure-breakdown", "Breakdown"), ("--measure-throughput", "Throughput"), ("--habitat-predict", "Habitat predict"), ("--measure-utilization", "Utilization"), ("--energy-compute", "Energy")], required=False)
+    exclude_source_files = serializers.ChoiceField(choices=[("include_files", "Add Encrypted Files"), ("exclude_files", "Exclude source files")], default="include_files", required=False)
     entry_point = serializers.CharField(
         max_length=100, required=False, default="entry_point.py"
     )
@@ -35,8 +37,25 @@ class ProjectUploadSerializer(serializers.Serializer):
         try:
             with zipfile.ZipFile(file_path, "r") as zip_ref:
                 zip_ref.extractall(tmp_dir)
-        except:  # noqa: E722
+        except:
             log.exception(f"Error while unzipping project archive: {file_path}")
             raise ValidationError("Invalid project archive")
 
         return tmp_dir_obj
+    
+    def resolve_entry_point_path(self): 
+        #Save and unzip project archive
+        tmp_dir_obj = self.save()
+        tmp_dir = tmp_dir_obj.name
+
+        # Lookup for entry point file 
+        entry_point_path = ""
+        for root, _, files in os.walk(tmp_dir):
+            for file in files: 
+                if file == self.validated_data['entry_point']:
+                    entry_point_path = os.path.join(root, file)
+                    log.debug("Found entry point file: %s", entry_point_path)
+                    break
+
+        return entry_point_path
+           
--- webservice/api/views.py
+++ webservice/api/views.py
@@ -1,19 +1,24 @@
-import logging
 import os
 import re
-import sqlite3
 import subprocess
 import sys
+import logging
+import sqlite3
+import json
+import datetime
 
 import pkg_resources
 import torch
+
 from django.http import FileResponse
+
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser
+
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import status
-from rest_framework.decorators import api_view, parser_classes
-from rest_framework.parsers import MultiPartParser
-from rest_framework.response import Response
 
 from webservice.api.serializers import ProjectUploadSerializer
 
@@ -64,22 +69,13 @@ def run_profiling(request):
     Mode - profiling mode (time or memory)
     Entry point - Name of the entry point .py file. Must be in the root folder of the project (e.g. entry_point.py)
     Output - output format (json or sqlite)
-    """  # noqa: E501
+    """
     serializer = ProjectUploadSerializer(data=request.data)
 
     if serializer.is_valid():
-        # Save and unzip projcet archive
-        tmp_dir_obj = serializer.save()
-        tmp_dir = tmp_dir_obj.name
-
+        # Save and unzip project archive
         # Look up for entry point file
-        entry_point_path = ""
-        for root, _, files in os.walk(tmp_dir):
-            for file in files:
-                if file == serializer.validated_data["entry_point"]:
-                    entry_point_path = os.path.join(root, file)
-                    log.debug("Found entry point file: %s", entry_point_path)
-                    break
+        entry_point_path = serializer.resolve_entry_point_path()
 
         if entry_point_path == "":
             return Response(
@@ -94,105 +90,187 @@ def run_profiling(request):
         # This condition made because DeepView.Profile fails working with absolute paths
         work_dir = os.path.dirname(entry_point_path)
 
-        # Running DeepView.Profile module
-        deepview_output_file = "profile_output.sqlite"
-        run_params = [
-            "python",
-            "-m",
-            "deepview_profile",
-            serializer.validated_data["mode"],
-            serializer.validated_data["entry_point"],
-            "-o",
-            deepview_output_file,
-        ]
+        if serializer.validated_data['mode'] == "time" or serializer.validated_data['mode'] == "memory":
+            deepview_output_file = "profile_output.sqlite"
 
-        log.debug("Working directory: %s", work_dir)
-        log.info("Running DeepView.Profile with params: %s", run_params)
+            exit_code, output, error_output = profile_time_and_memory(serializer, work_dir, deepview_output_file)
 
-        process_result = subprocess.run(
-            run_params, cwd=work_dir, capture_output=True, check=False
-        )
-        exit_code = process_result.returncode
-        output = process_result.stdout.decode("utf-8")
-        error_output = process_result.stderr.decode("utf-8")
+            # Put profiling result file to response
+            deepview_output_file_path = os.path.join(work_dir, deepview_output_file)
 
-        log.debug("DeepView.Profile exit code: %s", exit_code)
-        log.debug("DeepView.Profile output: %s", output)
-        log.debug("DeepView.Profile error output: %s", error_output)
+            if not os.path.exists(deepview_output_file_path):
+                log.error("Output file not found: %s", deepview_output_file_path)
+                return Response(
+                    {
+                        "error": "No output results found",
+                        "exit_code": exit_code,
+                        "output": output,
+                        "errors": error_output,
+                    },
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
 
-        # Put profiling result file to response
-        deepview_output_file_path = os.path.join(work_dir, deepview_output_file)
+            # Process sqlite output format
+            if serializer.validated_data["output"] == "sqlite":
+                file_name = os.path.basename(deepview_output_file_path)
+                response = FileResponse(open(deepview_output_file_path, "rb"))
+                response["Content-Disposition"] = f'attachment; filename="{file_name}"'
 
-        if not os.path.exists(deepview_output_file_path):
-            log.error("Output file not found: %s", deepview_output_file_path)
-            return Response(
-                {
-                    "error": "No output results found",
-                    "exit_code": exit_code,
-                    "output": output,
-                    "errors": error_output,
-                },
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+                # Remove special symbols from output
+                output = re.sub(r"[^\w\s]", "", output).replace("\n", "")
+                error_output = re.sub(r"[^\w\s]", "", error_output).replace("\n", "")
 
-        # Process sqlite output format
-        if serializer.validated_data["output"] == "sqlite":
-            file_name = os.path.basename(deepview_output_file_path)
-            response = FileResponse(open(deepview_output_file_path, "rb"))
-            response["Content-Disposition"] = f'attachment; filename="{file_name}"'
+                response["X-Deepview-ExitCode"] = exit_code
+                response["X-Deepview-Output"] = output
+                response["X-Deepview-Errors"] = error_output
 
-            # Remove special symbols from output
-            output = re.sub(r"[^\w\s]", "", output).replace("\n", "")
-            error_output = re.sub(r"[^\w\s]", "", error_output).replace("\n", "")
+                return response
 
-            response["X-Deepview-ExitCode"] = exit_code
-            response["X-Deepview-Output"] = output
-            response["X-Deepview-Errors"] = error_output
+            # Else, process json output format
+            db = sqlite3.connect(
+                deepview_output_file_path
+            )  # Replace with your actual database file path
+            cursor = db.cursor()
 
-            return response
-
-        # Else, process json output format
-        db = sqlite3.connect(
-            deepview_output_file_path
-        )  # Replace with your actual database file path
-        cursor = db.cursor()
-
-        # Get all tables in output file
-        log.debug("Loop through tables in output sqlite result file")
-        query = "SELECT name FROM sqlite_master WHERE type='table';"
-        cursor.execute(query)
-        tables = cursor.fetchall()
-
-        tables_dict = {}
-        for row in tables:
-            log.debug("Processing table: %s", {row[0]})
-            query = f"SELECT * FROM {row[0]};"
+            # Get all tables in output file
+            log.debug("Loop through tables in output sqlite result file")
+            query = "SELECT name FROM sqlite_master WHERE type='table';"
             cursor.execute(query)
-            results = cursor.fetchall()
+            tables = cursor.fetchall()
 
-            column_names = [description[0] for description in cursor.description]
-            rows_as_dict = [dict(zip(column_names, row)) for row in results]
-            tables_dict[row[0]] = rows_as_dict
+            tables_dict = {}
+            for row in tables:
+                log.debug("Processing table: %s", {row[0]})
+                query = f"SELECT * FROM {row[0]};"
+                cursor.execute(query)
+                results = cursor.fetchall()
 
-        cursor.close()
-        db.close()
+                column_names = [description[0] for description in cursor.description]
+                rows_as_dict = [dict(zip(column_names, row)) for row in results]
+                tables_dict[row[0]] = rows_as_dict
 
-        return Response(tables_dict)
+            cursor.close()
+            db.close()
+
+            return Response(tables_dict)
+        elif serializer.validated_data['mode'] == "analysis":
+            deepview_output_file = "profile_output.json"
+
+            exit_code, output, error_output = profile_analysis(serializer, work_dir, deepview_output_file)
+
+            deepview_output_file_path = os.path.join(work_dir, deepview_output_file)
+
+            if not os.path.exists(deepview_output_file_path):
+                log.error("Output file not found: %s", deepview_output_file_path)
+                return Response(
+                    {
+                        "error": "No output results found", 
+                        "exit_code": exit_code,
+                        "output": output,
+                        "errors": error_output,
+                    },
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+            
+            try:
+                with open(deepview_output_file_path, "r") as profile_json:
+                    json_data = json.load(profile_json)
+
+                    return Response(json_data)
+            except:
+                log.error("Error loading json output: %s", deepview_output_file_path)
+                return Response(
+                    {
+                        "error": "Could not load json output file",
+                        "exit_code": exit_code,
+                        "output": output,
+                        "errors": f"Error loading json output file: {deepview_output_file_path}"
+                    }
+                )
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Running deepview_profile time and memory    
+def profile_time_and_memory(serializer, work_dir, deepview_output_file):
+    # Running DeepView.Profile module
+    run_params = [
+        "python",
+        "-m",
+        "deepview_profile",
+        serializer.validated_data["mode"],
+        serializer.validated_data["entry_point"],
+        "-o",
+        deepview_output_file,
+    ]
 
+    log.debug("Working directory: %s", work_dir)
+    log.info("Running DeepView.Profile with params: %s", run_params)
+
+    process_result = subprocess.run(
+        run_params, cwd=work_dir, capture_output=True, check=False
+    )
+    exit_code = process_result.returncode
+    output = process_result.stdout.decode("utf-8")
+    error_output = process_result.stderr.decode("utf-8")
+
+    log.debug("DeepView.Profile exit code: %s", exit_code)
+    log.debug("DeepView.Profile output: %s", output)
+    log.debug("DeepView.Profile error output: %s", error_output)
+
+    return exit_code, output, error_output
+
+# Running deepview_profile in analysis mode
+def profile_analysis(serializer, work_dir, deepview_output_file):
+    # Running DeepView.Profile module 
+    selected_analysis_type = "--all"
+    analysis_types = serializer.validated_data["analysis_types"]
+
+    if len(analysis_types) > 0:
+        selected_analysis_type = " ".join(analysis_types)
+
+    selected_no_files = " "
+    if serializer.validated_data["exclude_source_files"] == "exclude_files":
+        selected_no_files = "--exclude_source_files"
+
+    run_params = [
+        "python",
+        "-m",
+        "deepview_profile",
+        "analysis",
+        serializer.validated_data["entry_point"],
+        selected_analysis_type,
+        selected_no_files,
+        "-o",
+        deepview_output_file
+    ]
+    
+    log.debug("Working directory: %s", work_dir)
+    log.info("Running DeepView.Profile with param: %s", run_params)
+
+    process_result = subprocess.run(run_params, 
+                                    cwd=work_dir, capture_output=True, check=False)
+
+    exit_code = process_result.returncode
+    output = process_result.stdout.decode("utf-8")
+    error_output = process_result.stderr.decode("utf-8")
+
+    log.debug("DeepView.Profile exit code: %s", exit_code)
+    log.debug("DeepView.Profile output: %s", output)
+    log.debug("DeepView.Profile error output: %s", error_output)
+
+    return exit_code, output, error_output
+    
 @api_view()
-def get_status(request):
+def get_status(request): 
     """
     Get service status
 
     ---
-    """
+    """    
     expected_python_version = pkg_resources.parse_version("3.9")
     expected_cuda_version = pkg_resources.parse_version("11.7")
     expected_deepview_version = pkg_resources.parse_version("0.13.1")
-
+    
     # Check Python version
     python_version = pkg_resources.parse_version(sys.version.split()[0])
     python_status = f"OK ({python_version})"
@@ -217,9 +295,7 @@ def get_status(request):
 
     try:
         log.debug("Checking GPU driver version")
-        process_output = subprocess.run(
-            ["nvidia-smi"], capture_output=True, text=True, check=False
-        )
+        process_output = subprocess.run(["nvidia-smi"], capture_output=True, text=True, check=False)
 
         match = re.search(r"CUDA Version:\s*([\d.]+)", process_output.stdout)
         if match:
@@ -232,7 +308,7 @@ def get_status(request):
         else:
             gpu_driver = f"OK ({driver_cuda_version})"
 
-    except:  # noqa: E722
+    except: # pylint: disable=bare-except
         log.exception("Error while checking GPU driver version")
 
     # Check DeepView.Profile package version
